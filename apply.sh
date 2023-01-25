1991386ba94e9b291ea966d22cce4c8645043971
Kerry Su <me@sshockwave.net>
2023-01-25 19:21:40 +0800

    Feat(zsh) embed theme

--- apply.sh
+++ apply.sh
@@ -2,8 +2,13 @@
 set -ex
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(realpath "$SCRIPT_DIR")"
 SRC_DIR="$SCRIPT_DIR/src"
 
 cd ~
+
+mkdir -p .config
+test ! -e .config/dotfiles
+ln -s "$SCRIPT_DIR" .config/dotfiles
 
 ln -s "$SRC_DIR/zshrc" .zshrc
---
+++ src/zsh_custom/themes/basic.zsh-theme
@@ -0,0 +1,2 @@
+# Based on "cypher" theme
+PROMPT="%m %{${fg_bold[red]}%}:: %{${fg[green]}%}%3~%(0?. . %{${fg[red]}%}%? )%{${fg[blue]}%}»%{${reset_color}%} "
