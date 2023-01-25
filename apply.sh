c39cf9b556c8c2031a36f2dccf6e086fa87f9517
Kerry Su <me@sshockwave.net>
2023-01-25 19:37:45 +0800

    Feat(conda) put inside directory

--- apply.sh
+++ apply.sh
@@ -3,12 +3,14 @@ set -ex
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SCRIPT_DIR="$(realpath "$SCRIPT_DIR")"
-SRC_DIR="$SCRIPT_DIR/src"
 
 cd ~
 
 mkdir -p .config
 test ! -e .config/dotfiles
-ln -s "$SCRIPT_DIR" .config/dotfiles
+ln -s "$SCRIPT_DIR/src" .config/dotfiles
+
+SRC_DIR="${HOME}/.config/dotfiles/"
 
 ln -s "$SRC_DIR/zshrc" .zshrc
+ln -s "$SRC_DIR/condarc.yml" .condarc
